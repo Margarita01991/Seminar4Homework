dc0ce8260409485a3d1f11c332a42ac882af634e
Margarita <margarita27011991@gmail.com>
2022-08-19 22:41:11 +0300

    выдаёт сумму цифр в числе

--- task27/Program.cs
+++ task27/Program.cs
@@ -3,8 +3,9 @@ Console.Clear();
 Console.WriteLine("Введите число : ");
 int number = Convert.ToInt32(Console.ReadLine());
 int sum = 0;
-for (int i = number; i > 0; i = i / 10)
+for (int i = number; i != 0; i = i / 10)
 {
 sum = sum + i % 10;
+//Console.WriteLine($"Сумма цифр в числе равна {i} | {i / 10} | {i % 10} |  {sum}");
 }
 Console.WriteLine($"Сумма цифр в числе равна {sum}");
