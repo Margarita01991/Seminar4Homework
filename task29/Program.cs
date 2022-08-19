a6f08e189bba7a7a595f3fb107053f483435b609
Margarita <margarita27011991@gmail.com>
2022-08-19 17:09:39 +0300

    задаёт массив из 8 элементов и выводит их на экран

--- task29/Program.cs
+++ task29/Program.cs
@@ -1,1 +1,13 @@
 ﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19] ;   6, 1, 33 -> [6, 1, 33]
+Console.Clear();
+// Console.WriteLine("Введите восьмизначное число");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[8];
+Random rand = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(0,100);
+}
+Console.WriteLine('[' + string.Join(",", array) + ']');
