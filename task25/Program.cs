535a2e3f4ec9924d35cec98dc384d2271d3f77c5
Margarita <margarita27011991@gmail.com>
2022-08-19 18:38:35 +0300

    возводит число A в натуральную степень B

--- task25/Program.cs
+++ task25/Program.cs
@@ -1,16 +1,13 @@
 ﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. 3, 5 -> 243 (3⁵); 2, 4 -> 16
 Console.Clear();
-int Degree(int A, int B)
-{
-    int result = 1;
-    for(int i = 1; i <= B; i++)
-    {
-        result = result * A;                              
-    }
-        return result; 
-}
 Console.WriteLine("введите число A ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите число B ");
 int numberB = Convert.ToInt32(Console.ReadLine());
-Console.Write($"Результат работы программы = {Degree(numberA, numberB)}");
+int result = 1;
+for(int i = 1; i <= numberB; i++)
+{
+    result = result * numberA;                                 
+}
+Console.Write(result);
+//Console.Write($"Результат работы программы = {result)}");
